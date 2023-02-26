13c8c2da5c71d3b17233a7ce1629f8ba1b96894c
RayaanUddin <rayaan.uddin@outlook.com>
2023-02-26 17:33:07 +0000

    main

---
+++ [sounds]/soundpack/__resource.lua
@@ -0,0 +1,438 @@
+fx_version 'adamant'
+game 'gta5'
+
+lua54 'on'
+
+server_scripts {
+	"server.lua"
+}
+client_scripts {
+	"client.lua",
+}
+files {   
+	'audioconfig/488sound_game.dat151.rel',
+	'audioconfig/488sound_sounds.dat54.rel',
+	'sfx/dlc_488sound/488sound.awc',
+	'sfx/dlc_488sound/488sound_npc.awc',
+	'audioconfig/apollosv8_game.dat151.rel',
+	'audioconfig/apollosv8_sounds.dat54.rel',
+	'sfx/dlc_apollosv8/apollosv8.awc',
+	'sfx/dlc_apollosv8/apollosv8_npc.awc',
+	'audioconfig/avesvv12_game.dat151.rel',
+	'audioconfig/avesvv12_sounds.dat54.rel',
+	'sfx/dlc_avesvv12/avesvv12.awc',
+	'sfx/dlc_avesvv12/avesvv12_npc.awc',
+	'audioconfig/c6v8sound_game.dat151.rel',
+	'audioconfig/c6v8sound_sounds.dat54.rel',
+	'sfx/dlc_c6v8sound/c6v8sound.awc',
+	'sfx/dlc_c6v8sound/c6v8sound_npc.awc',
+	'audioconfig/diablov12_game.dat151.nametable',
+	'audioconfig/diablov12_game.dat151.rel',
+	'audioconfig/diablov12_sounds.dat54.nametable',
+	'audioconfig/diablov12_sounds.dat54.rel',
+	'sfx/dlc_diablov12/diablov12.awc',
+	'sfx/dlc_diablov12/diablov12_npc.awc',
+	'audioconfig/f40v8_amp.dat10.rel',
+	'audioconfig/f40v8_game.dat151.rel',
+	'audioconfig/f40v8_sounds.dat54.rel',
+	'sfx/dlc_f40v8/f40v8.awc',
+	'sfx/dlc_f40v8/f40v8_npc.awc',
+	'audioconfig/f50v12_game.dat151.rel',
+	'audioconfig/f50v12_sounds.dat54.rel',
+	'sfx/dlc_f50v12/f50v12.awc',
+	'sfx/dlc_f50v12/f50v12_npc.awc',
+	'audioconfig/ferrarif12_amp.dat10.nametable',
+	'audioconfig/ferrarif12_amp.dat10.rel',
+	'audioconfig/ferrarif12_game.dat151.nametable',
+	'audioconfig/ferrarif12_game.dat151.rel',
+	'audioconfig/ferrarif12_sounds.dat54.nametable',
+	'audioconfig/ferrarif12_sounds.dat54.rel',
+	'sfx/dlc_ferrarif12/ferrarif12.awc',
+	'sfx/dlc_ferrarif12/ferrarif12_npc.awc',
+	'audioconfig/gtaspanov10_amp.dat10.nametable',
+	'audioconfig/gtaspanov10_amp.dat10.rel',
+	'audioconfig/gtaspanov10_game.dat151.nametable',
+	'audioconfig/gtaspanov10_game.dat151.rel',
+	'audioconfig/gtaspanov10_sounds.dat54.nametable',
+	'audioconfig/gtaspanov10_sounds.dat54.rel',
+	'sfx/dlc_gtaspanov10/gtaspanov10.awc',
+	'sfx/dlc_gtaspanov10/gtaspanov10_npc.awc',
+	'audioconfig/mclarenv8_amp.dat10.nametable',
+	'audioconfig/mclarenv8_amp.dat10.rel',
+	'audioconfig/mclarenv8_game.dat151.nametable',
+	'audioconfig/mclarenv8_game.dat151.rel',
+	'audioconfig/mclarenv8_sounds.dat54.nametable',
+	'audioconfig/mclarenv8_sounds.dat54.rel',
+	'sfx/dlc_mclarenv8/mclarenv8.awc',
+	'sfx/dlc_mclarenv8/mclarenv8_npc.awc',
+	'audioconfig/murciev12_amp.dat10.nametable',
+	'audioconfig/murciev12_amp.dat10.rel',
+	'audioconfig/murciev12_game.dat151.nametable',
+	'audioconfig/murciev12_game.dat151.rel',
+	'audioconfig/murciev12_sounds.dat54.nametable',
+	'audioconfig/murciev12_sounds.dat54.rel',
+	'sfx/dlc_murciev12/murciev12.awc',
+	'sfx/dlc_murciev12/murciev12_npc.awc',
+	'audioconfig/perfov10_amp.dat10.nametable',
+	'audioconfig/perfov10_amp.dat10.rel',
+	'audioconfig/perfov10_game.dat151.nametable',
+	'audioconfig/perfov10_game.dat151.rel',
+	'audioconfig/perfov10_sounds.dat54.nametable',
+	'audioconfig/perfov10_sounds.dat54.rel',
+	'sfx/dlc_perfov10/perfov10.awc',
+	'sfx/dlc_perfov10/perfov10_npc.awc',
+	'audioconfig/sestov10_amp.dat10.nametable',
+	'audioconfig/sestov10_amp.dat10.rel',
+	'audioconfig/sestov10_game.dat151.nametable',
+	'audioconfig/sestov10_game.dat151.rel',
+	'audioconfig/sestov10_sounds.dat54.nametable',
+	'audioconfig/sestov10_sounds.dat54.rel',
+	'sfx/dlc_sestov10/sestov10.awc',
+	'sfx/dlc_sestov10/sestov10_npc.awc',
+	'audioconfig/urusv8_game.dat151.nametable',
+	'audioconfig/urusv8_game.dat151.rel',
+	'audioconfig/urusv8_sounds.dat54.nametable',
+	'audioconfig/urusv8_sounds.dat54.rel',
+	'sfx/dlc_urusv8/urusv8.awc',
+	'sfx/dlc_urusv8/urusv8_npc.awc',
+	'audioconfig/venenov12_game.dat151.rel',
+	'audioconfig/venenov12_sounds.dat54.rel',
+	'sfx/dlc_venenov12/venenov12.awc',
+	'sfx/dlc_venenov12/venenov12_npc.awc',
+	'audioconfig/viperv10_game.dat151.rel',
+	'audioconfig/viperv10_sounds.dat54.rel',
+	'sfx/dlc_viperv10/viperv10.awc',
+	'sfx/dlc_viperv10/viperv10_npc.awc',
+	'audioconfig/lambov10_game.dat151.rel',
+	'audioconfig/lambov10_sounds.dat54.rel',
+	'sfx/dlc_lambov10/lambov10.awc',
+	'sfx/dlc_lambov10/lambov10_npc.awc',
+	'audioconfig/musv8_game.dat151.rel',
+	'audioconfig/musv8_sounds.dat54.rel',
+	'sfx/dlc_musv8/musv8.awc',
+	'sfx/dlc_musv8/musv8_npc.awc',
+	'audioconfig/brabus850_game.dat151.rel',
+	'audioconfig/brabus850_sounds.dat54.rel',
+	'sfx/dlc_brabus850/brabus850.awc',
+	'sfx/dlc_brabus850/brabus850_npc.awc',
+	'audioconfig/shonen_game.dat151.rel',
+	'audioconfig/shonen_sounds.dat54.rel',
+	'sfx/dlc_shonen/shonen.awc',
+	'sfx/dlc_shonen/shonen_npc.awc',
+	'audioconfig/toysupmk4_game.dat151.nametable',
+	'audioconfig/toysupmk4_game.dat151.rel',
+	'audioconfig/toysupmk4_sounds.dat54.nametable',
+	'audioconfig/toysupmk4_sounds.dat54.rel',
+	'sfx/dlc_toysupmk4/toysupmk4.awc',
+	'sfx/dlc_toysupmk4/toysupmk4_npc.awc',
+	'audioconfig/rb26dett_amp.dat10.rel',
+	'audioconfig/rb26dett_game.dat151.rel',
+	'audioconfig/rb26dett_sounds.dat54.rel',
+	'sfx/dlc_rb26dett/rb26dett.awc',
+	'sfx/dlc_rb26dett/rb26dett_npc.awc',
+	'audioconfig/rotary7_game.dat151.rel',
+	'audioconfig/rotary7_sounds.dat54.rel',
+	'sfx/dlc_rotary7/rotary7.awc',
+	'sfx/dlc_rotary7/rotary7_npc.awc',
+	'audioconfig/m297zonda_amp.dat10.nametable',
+	'audioconfig/m297zonda_amp.dat10.rel',
+	'audioconfig/m297zonda_game.dat151.nametable',
+	'audioconfig/m297zonda_game.dat151.rel',
+	'audioconfig/m297zonda_sounds.dat54.nametable',
+	'audioconfig/m297zonda_sounds.dat54.rel',
+	'sfx/dlc_m297zonda/m297zonda.awc',
+	'sfx/dlc_m297zonda/m297zonda_npc.awc',
+	'audioconfig/m158huayra_amp.dat10.nametable',
+	'audioconfig/m158huayra_amp.dat10.rel',
+	'audioconfig/m158huayra_game.dat151.nametable',
+	'audioconfig/m158huayra_game.dat151.rel',
+	'audioconfig/m158huayra_sounds.dat54.nametable',
+	'audioconfig/m158huayra_sounds.dat54.rel',
+	'sfx/dlc_m158huayra/m158huayra.awc',
+	'sfx/dlc_m158huayra/m158huayra_npc.awc',
+	'audioconfig/k20a_amp.dat10.nametable',
+	'audioconfig/k20a_amp.dat10.rel',
+	'audioconfig/k20a_game.dat151.nametable',
+	'audioconfig/k20a_game.dat151.rel',
+	'audioconfig/k20a_sounds.dat54.nametable',
+	'audioconfig/k20a_sounds.dat54.rel',
+	'sfx/dlc_k20a/k20a.awc',
+	'sfx/dlc_k20a/k20a_npc.awc',
+	'audioconfig/gt3flat6_amp.dat10.nametable',
+	'audioconfig/gt3flat6_amp.dat10.rel',
+	'audioconfig/gt3flat6_game.dat151.nametable',
+	'audioconfig/gt3flat6_game.dat151.rel',
+	'audioconfig/gt3flat6_sounds.dat54.nametable',
+	'audioconfig/gt3flat6_sounds.dat54.rel',
+	'sfx/dlc_gt3flat6/gt3flat6.awc',
+	'sfx/dlc_gt3flat6/gt3flat6_npc.awc',
+	'audioconfig/predatorv8_amp.dat10.nametable',
+	'audioconfig/predatorv8_amp.dat10.rel',
+	'audioconfig/predatorv8_game.dat151.nametable',
+	'audioconfig/predatorv8_game.dat151.rel',
+	'audioconfig/predatorv8_sounds.dat54.nametable',
+	'audioconfig/predatorv8_sounds.dat54.rel',
+	'sfx/dlc_predatorv8/predatorv8.awc',
+	'sfx/dlc_predatorv8/predatorv8_npc.awc',
+	'audioconfig/p60b40_amp.dat10.nametable',
+	'audioconfig/p60b40_amp.dat10.rel',
+	'audioconfig/p60b40_game.dat151.nametable',
+	'audioconfig/p60b40_game.dat151.rel',
+	'audioconfig/p60b40_sounds.dat54.nametable',
+	'audioconfig/p60b40_sounds.dat54.rel',
+	'sfx/dlc_p60b40/p60b40.awc',
+	'sfx/dlc_p60b40/p60b40_npc.awc',
+	'audioconfig/demonv8_game.dat151.nametable',
+	'audioconfig/demonv8_game.dat151.rel',
+	'audioconfig/demonv8_sounds.dat54.nametable',
+	'audioconfig/demonv8_sounds.dat54.rel',
+	'sfx/dlc_demonv8/demonv8.awc',
+	'sfx/dlc_demonv8/demonv8_npc.awc',
+	'audioconfig/nfsv8_game.dat151.rel',
+	'audioconfig/nfsv8_sounds.dat54.rel',
+	'sfx/dlc_nfsv8/nfsv8.awc',
+	'sfx/dlc_nfsv8/nfsv8_npc.awc',
+	'audioconfig/r35sound_game.dat151.rel',
+	'audioconfig/r35sound_sounds.dat54.rel',
+	'sfx/dlc_r35sound/r35sound.awc',
+	'sfx/dlc_r35sound/r35sound_npc.awc',
+	'audioconfig/m5cracklemod_game.dat151.rel',
+	'audioconfig/m5cracklemod_sounds.dat54.rel',
+	'sfx/dlc_m5cracklemod/m5cracklemod.awc',
+	'sfx/dlc_m5cracklemod/m5cracklemod_npc.awc',
+	'audioconfig/avesv_game.dat151.rel',
+	'audioconfig/avesv_sounds.dat54.rel',
+	'sfx/dlc_avesv/avesv.awc',
+	'sfx/dlc_avesv/avesv_npc.awc',
+	'audioconfig/agerarsv2_game.dat151.rel',
+	'audioconfig/agerarsv2_sounds.dat54.rel',
+	'sfx/dlc_agerarsv2/agerarsv2.awc',
+	'sfx/dlc_agerarsv2/agerarsv2_npc.awc', 
+	'audioconfig/agerars_game.dat151.rel',
+	'audioconfig/agerars_sounds.dat54.rel',
+	'sfx/dlc_agerars/agerars.awc',
+	'audioconfig/dlcfcyote_game.dat151.rel',
+	'audioconfig/dlcfcyote_sounds.dat54.rel',
+	'sfx/dlc_fcyote/fordcoyote.awc',
+	'sfx/dlc_fcyote/fordcoyote_npc.awc',
+	'audioconfig/majimalm_game.dat151.rel',
+	'audioconfig/majimalm_sounds.dat54.rel',
+	'sfx/dlc_majimagt/majimagt.awc',
+	'sfx/dlc_majimagt/majimagt_npc.awc',
+	'audioconfig/w211_game.dat151.rel',
+	'audioconfig/w211_sounds.dat54.rel',
+	'sfx/dlc_w211/w211.awc',
+	'sfx/dlc_w211/w211_npc.awc',
+	'audioconfig/53vortec_game.dat151.rel',
+	'audioconfig/53vortec_sounds.dat54.rel',
+	'sfx/dlc_53vortec/53vortec.awc',
+	'audioconfig/64powerstroke_game.dat151.rel',
+	'audioconfig/64powerstroke_sounds.dat54.rel',
+	'sfx/dlc_64powerstroke/64powerstroke.awc',
+	'audioconfig/46fordmodular_game.dat151.rel',
+	'audioconfig/46fordmodular_sounds.dat54.rel',
+	'sfx/dlc_46fordmodular/46fordmodular.awc',
+	'audioconfig/12vcummins_game.dat151.rel',
+	'audioconfig/12vcummins_sounds.dat54.rel',
+	'sfx/dlc_12vcummins/12vcummins.awc',
+	'sfx/dlc_12vcummins/12vcummins_npc.awc',
+	'audioconfig/cw2019_game.dat151',
+	'audioconfig/cw2019_game.dat151.nametable',
+	'audioconfig/cw2019_game.dat151.rel',
+	'audioconfig/cw2019_sounds.dat54',
+	'audioconfig/cw2019_sounds.dat54.nametable',
+	'audioconfig/cw2019_sounds.dat54.rel',
+	'sfx/dlc_cw2019/cw2019.awc',
+	'sfx/dlc_cw2019/cw2019_npc.awc',
+	'audioconfig/vr38dettv6_game.dat151.rel',
+	'audioconfig/vr38dettv6_sounds.dat54.rel',
+	'sfx/dlc_vr38dettv6/vr38dettv6.awc',
+	'sfx/dlc_vr38dettv6/vr38dettv6_npc.awc',
+	'audioconfig/novitecsvj_game.dat151.rel',
+	'audioconfig/novitecsvj_sounds.dat54.rel',
+	'sfx/dlc_novitecsvj/novitecsvj.awc',
+	'sfx/dlc_novitecsvj/novitecsvj_npc.awc',
+	'audioconfig/s63b44_game.dat151.rel',
+	'audioconfig/s63b44_sounds.dat54.rel',
+	'sfx/dlc_s63b44/s63b44.awc',
+	'sfx/dlc_s63b44/s63b44_npc.awc',
+	'audioconfig/ml720v8eng_game.dat151.rel',
+	'audioconfig/ml720v8eng_sounds.dat54.rel',
+	'sfx/dlc_ml720v8eng/ml720v8eng.awc',
+	'sfx/dlc_ml720v8eng/ml720v8eng_npc.awc'
+}
+
+-- Car Sounds
+data_file 'AUDIO_GAMEDATA' 'audioconfig/488sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/488sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_488sound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/apollosv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/apollosv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_apollosv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/avesvv12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/avesvv12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_avesvv12'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/c6v8sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/c6v8sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_c6v8sound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/diablov12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/diablov12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_diablov12'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/f40v8_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f40v8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f40v8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f40v8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f50v12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f50v12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f50v12'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/ferrarif12_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ferrarif12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ferrarif12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ferrarif12'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gtaspanov10_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gtaspanov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gtaspanov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gtaspanov10'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/mclarenv8_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mclarenv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mclarenv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mclarenv8'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/murciev12_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/murciev12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/murciev12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_murciev12'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/perfov10_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/perfov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/perfov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_perfov10'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/sestov10_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/sestov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/sestov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_sestov10'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/urusv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/urusv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_urusv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/venenov12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/venenov12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_venenov12'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/viperv10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/viperv10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_viperv10'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lambov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lambov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lambov10'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/musv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/musv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_musv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/brabus850_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/brabus850_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_brabus850'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/shonen_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/shonen_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_shonen'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/toysupmk4_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/toysupmk4_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_toysupmk4'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/rb26dett_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rotary7_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rotary7_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rotary7'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/m297zonda_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/m297zonda_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/m297zonda_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m297zonda'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/m158huayra_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/m158huayra_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/m158huayra_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m158huayra'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/k20a_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/k20a_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/k20a_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_k20a'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/gt3flat6_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/gt3flat6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/gt3flat6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_gt3flat6'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/predatorv8_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/predatorv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/predatorv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_predatorv8'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/p60b40_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/p60b40_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/p60b40_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_p60b40'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/demonv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/demonv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_demonv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/nfsv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/nfsv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_nfsv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/r35sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/r35sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_r35sound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/m5cracklemod_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/m5cracklemod_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_m5cracklemod'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/avesv_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/avesv_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_avesv'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/agerarsv2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/agerarsv2_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_agerarsv2'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/agerars_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/agerars_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_agerars'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/dlcfcyote_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/dlcfcyote_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_fcyote'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/majimalm_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/majimalm_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_majimagt'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/w211_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/w211_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_w211'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/53vortec_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/53vortec_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_53vortec'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/64powerstroke_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/64powerstroke_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_64powerstroke'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/46fordmodular_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/46fordmodular_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_46fordmodular'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/12vcummins_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/12vcummins_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_12vcummins'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/vr38dettv6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/vr38dettv6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_vr38dettv6'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/novitecsvj_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/novitecsvj_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_novitecsvj'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s63b44_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s63b44_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s63b44'
+data_file "AUDIO_GAMEDATA" 'audioconfig/ml720v8eng_game.dat'
+data_file "AUDIO_SOUNDDATA" 'audioconfig/ml720v8eng_sounds.dat'
+data_file "AUDIO_WAVEPACK" 'sfx/dlc_ml720v8eng'
+--data_file "AUDIO_WAVEPACK" "sfx/dlc_RufusVanquish"
+--data_file "AUDIO_WAVEPACK" "sfx/dlc_rmgtr35"
+--data_file "AUDIO_WAVEPACK" "sfx/dlc_hotwlambo"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
